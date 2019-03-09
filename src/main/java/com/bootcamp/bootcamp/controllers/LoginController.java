fec297d26360540b2379ed442a93712b50554eb5
pawel <pawelmaciaszek@gmail.com>
2019-03-09 20:42:10 +0100

    security/login to admin

---
+++ src/main/java/com/bootcamp/bootcamp/controllers/LoginController.java
@@ -0,0 +1,15 @@
+package com.bootcamp.bootcamp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//
+//public class LoginController {
+//
+//    @GetMapping("/wyloguj")
+//    public String logout () {
+//        return "/";
+//    }
+//
+//}
